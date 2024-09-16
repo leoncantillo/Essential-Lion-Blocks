05e86cbee26160e7d2d3baf886268313e934a858
León Cantillo <leonjob13@gmail.com>
2024-09-16 13:02:06 -0500

    [ADD] initial plugin information

---
+++ essential-lion-blocks.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:  Essential Lion Blocks
+ * Description:  Plugin to display custom blocks with shortcodes
+ * Version:      1.0
+ * Author:       Leon Cantillo
+ * Author URI:   https://github.com/LeonCantillo
+ * License:      MIT
+ * License URI:  https://opensource.org/licenses/MIT
+ * Text Domain:  Essential-Blocks
+ * Domain Path:  /languages
+ * Charset:      UTF-8
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
